a790cb5455b6abe1d7d7361918fddd250f6780ac
Lokesh R <lokesh.r1206@gmail.com>
2026-06-17 17:53:48 +0530

    Time: 842 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,8 +1,4 @@
-SELECT 
-    Person.firstName,
-    Person.lastName,
-    Address.city,
-    Address.state
-FROM Person
-LEFT JOIN Address
-    ON Person.personId = Address.personId;
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
