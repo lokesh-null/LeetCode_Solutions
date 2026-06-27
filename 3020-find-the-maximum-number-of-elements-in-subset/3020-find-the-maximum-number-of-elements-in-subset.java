4926dcc966fca6e0d7095b442cd2c4611700bd6c
Lokesh R <lokesh.r1206@gmail.com>
2026-06-27 07:28:59 +0530

    Time: 41 ms (100%), Space: 64 MB (91.86%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
@@ -1,30 +1,31 @@
-class Solution {
+public class Solution {
     public int maximumLength(int[] nums) {
-        
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            map.merge(num, 1, Integer::sum);
         }
-        int max = map.containsKey(1) ? (map.get(1) % 2 == 0 ? map.get(1) - 1 : map.get(1)) : 1;
-
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            int val = nums[i];
-            
-            while (map.containsKey(val) && map.get(val) >= 2 && val != 1) {
-                count += 2;
-                val *= val;
-            }
-            
-            if (map.containsKey(val)) {
-                count++;
-            } else {
-                count--;
-            }
-            
-            max = Math.max(count, max);
+        int ans = map.getOrDefault(1, 1);
+        if (ans % 2 == 0) {
+            ans--;
         }
-        
-        return max;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getKey() == 1) {
+                continue;
+            }
+            if (entry.getValue() >= 2) {
+                int c = 1;
+                long a = entry.getKey();
+                a *= a;
+                while (a < Integer.MAX_VALUE && map.getOrDefault((int) a, 0) >= 2) {
+                    a *= a;
+                    c++;
+                }
+                if (a < Integer.MAX_VALUE && map.getOrDefault((int) a, 0) == 1) {
+                    c++;
+                }
+                ans = Math.max(ans, 2 * c - 1);
+            }
+        }
+        return ans;
     }
 }
