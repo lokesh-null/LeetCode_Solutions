a8c586faae373668e39dd3dabfca4256edafc0c8
Lokesh R <lokesh.r1206@gmail.com>
2026-07-07 14:24:24 +0530

    Time: 5 ms (87.64%), Space: 47.7 MB (86.02%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,26 +1,29 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        int rows = img.length;
-        int columns = img[0].length;
-        int[][] result = new int[rows][columns];
-
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                int totalSum = 0;
-                int count = 0;
-
-                for (int x = Math.max(0, i-1); x < Math.min(rows, i+2); ++x) {
-                    for (int y = Math.max(0, j-1); y < Math.min(columns, j+2); ++y) {
-                        totalSum += img[x][y];
-                        count += 1;
-                    }
-                }
-
-                result[i][j] = totalSum / count;
+        int res[][] = new int[img.length][img[0].length];
+        for(int i = 0; i < img.length; i++) {
+            for(int j = 0; j < img[0].length; j++) {
+                res[i][j] = smoothen(img, i, j);
             }
         }
-
-        return result;
+        return res;
     }
     
+    int smoothen(int[][] img, int x, int y) {
+        int m = img.length; 
+        int n = img[0].length;
+        int sum = 0;
+        int count = 0;
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                int nx = x + i;
+                int ny = y + j;
+                if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                sum += img[nx][ny];
+                count++;
+            }
+        }
+        return sum/count;
+        
+    }
 }
