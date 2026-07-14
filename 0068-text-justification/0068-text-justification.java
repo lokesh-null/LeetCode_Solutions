bdd946bbfe0110b187af8b029e5b1d69053acf3d
Lokesh R <lokesh.r1206@gmail.com>
2026-07-14 15:06:13 +0530

    Time: 0 ms (100%), Space: 43.5 MB (13.58%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int index = 0;
+        int n = words.length;
+
+        while (index < n) {
+            int start = index;
+            int wordLengthSum = words[index].length();
+            index++;
+
+            while (index < n && wordLengthSum + 1 + words[index].length() <= maxWidth) {
+                wordLengthSum += 1 + words[index].length();
+                index++;
+            }
+
+            int wordCount = index - start;
+            int gapCount = wordCount - 1;
+            int totalSpaces = maxWidth - (wordLengthSum - gapCount);
+
+            StringBuilder line = new StringBuilder();
+
+            if (index == n || gapCount == 0) {
+                for (int k = start; k < index; k++) {
+                    if (k > start) line.append(" ");
+                    line.append(words[k]);
+                }
+                while (line.length() < maxWidth) line.append(" ");
+            } else {
+                int evenSpaces = totalSpaces / gapCount;
+                int extraSpaces = totalSpaces % gapCount;
+
+                for (int k = start; k < index; k++) {
+                    line.append(words[k]);
+                    if (k < index - 1) {
+                        int spacesHere = evenSpaces + (extraSpaces-- > 0 ? 1 : 0);
+                        line.append(" ".repeat(spacesHere));
+                    }
+                }
+            }
+            result.add(line.toString());
+        }
+        return result;
+    }
+}
